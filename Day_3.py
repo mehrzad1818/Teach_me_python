f1457f62e963e0b0bb6bf420cc9f7d2128825361
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-01-10 02:08:09 +0330

    Update Day_3.py

--- Day_3.py
+++ Day_3.py
@@ -139,13 +139,15 @@ weight = float(input("Enter your weight in kg: "))
 
 bmi = round(((weight // (height ** 2))))
 
-if bmi <= 18.5:
+if bmi < 18.5:
     print(f"Your BMI is {bmi}. You are underweight.")
-elif 18.5 < bmi <= 25:
+elif bmi < 25:
     print(f"Your BMI is {bmi}. You have a normal weight.")
-elif 25 < bmi <= 30:
+elif bmi < 30:
     print(f"Your BMI is {bmi}. You are overweight.")
-elif 30 < bmi <= 35:
+elif bmi < 35:
     print(f"Your BMI is {bmi}. You are obese.")
 else:
     print(f"Your BMI is {bmi}. You are clinically obese.")
+
+# Code challenge 3.3 Leap year calculator
