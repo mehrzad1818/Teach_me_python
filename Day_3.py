c80a5246d481c58bec2562b1b8031a70cee8d9de
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-01-13 22:19:47 +0330

    Update Day_3.py

--- Day_3.py
+++ Day_3.py
@@ -239,29 +239,61 @@ if size == "S":
     bill += 15
     if add_pepperoni == "Y":
         bill += 2
-        print(f"Your bill is ${bill}.")
     if extra_cheese == "Y":
         bill += 1
         print(f"Your bill is ${bill}.")
     else:
-        print(f"Your bill is {bill}.")
+        print(f"Your bill is ${bill}.")
 if size == "M":
     bill += 20
     if add_pepperoni == "Y":
         bill += 3
-        print(f"Your bill is ${bill}.")
     if extra_cheese == "Y":
         bill += 1
         print(f"Your bill is ${bill}.")
     else:
-        print(f"Your bill is {bill}.")
+        print(f"Your bill is ${bill}.")
 if size == "L":
     bill += 25
     if add_pepperoni == "Y":
         bill += 3
-        print(f"Your bill is ${bill}.")
     if extra_cheese == "Y":
         bill += 1
         print(f"Your bill is ${bill}.")
     else:
-        print(f"Your bill is {bill}.")
+        print(f"Your bill is ${bill}.")
+else:
+    print("WRONG INPUT!")
+
+# The above code that I wrote comes from a less efficient way of thinking about it.
+# Instead of integrating functions (machines) inside each other, you can simply feed the product of one function (machine) into the next one.
+# The code below I believe is more efficient in terms of maintenance and comprehensibility.
+
+print("Welcome to Python Pizza Order Program (PPOP).\n")
+
+size = input("What size pizza do you want? S, M or L\n")
+add_pepperoni = input("Do you want pepperoni? Y or N\n")
+extra_cheese = input("Do you want extra cheese? Y or N\n")
+bill = 0
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your total bill is ${bill}")
+
+# Logical Operators
+
+# Now we are going to find about how to chain multiple conditions in the same line of code
