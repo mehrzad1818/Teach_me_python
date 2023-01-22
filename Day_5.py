42575c6a511700cdca498ec6560c5a2e7303ae91
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-01-22 23:41:58 +0330

    Update Day_5.py

--- Day_5.py
+++ Day_5.py
@@ -1,2 +1,4 @@
 # "This is Day 5 of One hundred days of coding
 # " For Loops, Range and Code Blocks"
+# The project of this day is to make a password generator
+# Its name is going to be PyPassword Generator
