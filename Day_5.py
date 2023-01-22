b5b24b0e501f668d971259e20cdff67ccc27092c
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-01-22 23:44:04 +0330

    Update Day_5.py

--- Day_5.py
+++ Day_5.py
@@ -2,3 +2,11 @@
 # " For Loops, Range and Code Blocks"
 # The project of this day is to make a password generator
 # Its name is going to be PyPassword Generator
+
+
+# First concept it
+
+# For Loop
+
+# for item in list_of_items:
+# Do something to each item
