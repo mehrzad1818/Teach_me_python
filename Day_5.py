b6e0a0534e1a9f1f990187aa4bda1165fa2e3e26
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-01-22 23:41:02 +0330

    Update Day_5.py

--- Day_5.py
+++ Day_5.py
@@ -1,3 +1,2 @@
-# This file contains challenges and codes from Day 5 of the 100 Days with Python
-#"This is Day 5 of One hundred days of coding
-# "
+# "This is Day 5 of One hundred days of coding
+# " For Loops, Range and Code Blocks"
