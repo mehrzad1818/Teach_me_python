9654b423f106109ff9fa9019a313269ed9f5f648
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-01-24 13:09:05 +0330

    Update Day_5.py

--- Day_5.py
+++ Day_5.py
@@ -16,3 +16,47 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
+print(fruits)
+# For is a loop, it goes over and assigns a variable name to your list, which
+# that assigned letter or name comes between for and in
+
+# Day 5.1 Average Height Exercise
+
+# "This code will add up the given input in integer and then divide it
+# by the number of given data to calculate its average."
+
+student_heights = input("Input a list of student heights ").split(", ")
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+print(student_heights)
+
+total_height = 0
+for height in student_heights:
+    total_height += height
+print(total_height)
+
+number_of_students = 0
+for student in student_heights:
+    number_of_students += 1
+print(number_of_students)
+
+average_height = round(total_height / number_of_students)
+print(average_height)
+
+
+# Modified
+
+
+student_heights = input(
+    "Write a list of numbers.\nDon't put comma between them, just a space\n").split()
+summy = 0
+dividi = 0
+for number in range(0, len(student_heights)):
+    student_heights[number] = int(student_heights[number])
+    summy += student_heights[number]
+    dividi += (student_heights[number] / student_heights[number])
+print(summy)
+print(dividi)
+
+average = (round(summy) / round(dividi))
+print(average)
