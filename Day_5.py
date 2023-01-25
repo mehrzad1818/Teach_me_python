63f0666b4ad1cb48eace3846d781542b16e0ca88
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-01-25 23:48:50 +0330

    Update Day_5.py

--- Day_5.py
+++ Day_5.py
@@ -75,3 +75,17 @@ for student_score in range(0, len(student_scores)):
     if value < student_scores[student_score]:
         value = student_scores[student_score]
 print(f"The highest score in the class is: {value}")
+
+# Above is the version I came up with, down below is the version proposed by the instructor
+
+student_scores = input("Input a list of student scores. \n").split()
+
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print(f"The highest score is: {highest_score}")
