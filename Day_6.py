14387c4cb2d6badd6fb28d262b9283c23c90cea1
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-01-26 23:43:37 +0330

    Update Day_6.py

--- Day_6.py
+++ Day_6.py
@@ -1,1 +1,13 @@
 
+# "This is the beginning of Day 6
+# We're going to learn about function, code blocks and while loop."
+
+# 1. Functions
+
+# Python has many buil-in functions, some basic examples are down below:
+
+print("Hello")
+print(len("Hello"))
+print(len(str(2345698)))
+
+# Now, we are going to learn how to "define" our own functions: 
