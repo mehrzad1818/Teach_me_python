8d004e4f52bd1af59ff8b659073a243628f0f5b3
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-01-28 19:12:58 +0330

    Update Day_6.py

--- Day_6.py
+++ Day_6.py
@@ -141,3 +141,32 @@ while not at_goal():
         jump()
     else:
         move()
+
+
+# Refined version is like this:
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    
+    while wall_on_right():
+        move()
+        
+    turn_right()
+    move()
+    turn_right()
+    
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+        
