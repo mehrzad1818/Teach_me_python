15d9076e090cccd20f2fb25e342b443b8e98230d
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-01-29 23:56:24 +0330

    Update Day_7.py

--- Day_7.py
+++ Day_7.py
@@ -1,3 +1,4 @@
 # This file contains challenges and new concepts learnt from Day 7 of 100 Days of Python Challenge.
 # We'll learn about many different things
 # We will learn how to make a hangman
+# Hangman is like a stickman which is not complete and you have to guess
