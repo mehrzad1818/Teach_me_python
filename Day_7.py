759e8d9f2e46fcb569d2e6d6d7d6e8cd39ee64e6
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-01-30 23:52:46 +0330

    Update Day_7.py

--- Day_7.py
+++ Day_7.py
@@ -1,20 +1,18 @@
-# This file contains challenges and new concepts learnt from Day 7 of 100 Days of Python Challenge.
-# We'll learn about many different things
-# We will learn how to make a hangman
-# Hangman is like a stickman which is not complete and you have to guess
-
 # Hello and good morning - This is Day 7 challenge of one hundred days of python and it deals with concepts we covered through the week
 # like; for and while loops, if/elif/else, strings, lists, range and modules
 
 # " So, the first step in making a working hangman is to
 #
+import random
 word_list = ["ardvark", "baboon", "camel"]
 
-# 1. Randomly choose a word from the given it
+# 1. Randomly choose a word from the given list
 # to a variable called something like chosen words."
 
-#"2. Ask the user to guess a letter and assign their answer
+# "2. Ask the user to guess a letter and assign their answer
 # to a variable called guess. Make guess lowercase."
 
-#"3. Check if the letter the user guessed is one of the
+# "3. Check if the letter the user guessed is one of the
 # letters in the chosen_word"
+
+for word in word_list(0, )
