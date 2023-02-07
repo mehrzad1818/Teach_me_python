66939a0cec4839662c339c2d2c6209b29f0ddd0d
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-07 23:07:53 +0330

    Update Day_8.py

--- Day_8.py
+++ Day_8.py
@@ -21,7 +21,43 @@ greet()
 # Like this:
 
 # def my_function(blah blah blah blah):
-#     # Do this
-#     # Do that
+#     # Do this to blah blah blah
+#     # Do that to the rest of blah blahs
 #     # Then do this and this
 #     # Done
+
+# In this way, we can change the function based on the given input
+
+# Here's an example that allows for input
+
+
+def greet_with_given_name(name):
+    print(f"Hello, {name}")
+    print(f"How are you {name}?")
+
+
+name = input("What is your name? ")
+
+greet_with_given_name(name)
+
+
+# In the example above, "name" is the parameter, and the for example "Jimmy" is the argument
+
+
+# This part is about functions with more than one input.
+
+def name_and_last(name, last):
+    print(f"Your name is: {name}")
+    print(f"Your last name is: {last}\n")
+
+    print(f"Hello, {name} {last}")
+    print(f"How are you {name} {last}")
+
+
+name = input("What is your name? ")
+location = input("Where are you from? ")
+
+
+def greet_with(name, location):
+    print(f"Hello, {name}")
+    print(f"What is it like in {location}?")
