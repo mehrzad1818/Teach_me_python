728ec108056f324e9648d39f01c77236a4b0e527
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-07 09:32:56 +0330

    Update Day_8.py

--- Day_8.py
+++ Day_8.py
@@ -1,2 +1,17 @@
 # This file contains projects and new concepts from Day 8 of 100 Days of Python
-# This files deals with Day 8
+# Day 8 is about function with capability of input, arguments and parameters
+# The final project is about making a ceasar cipher program which deals
+# encrypt and decrypt messages
+
+# def my_function():
+#     # Do this
+#     # Do that
+#     # Then do this and this
+#     # Done
+
+def greet():
+    print("Hello,")
+    print("Welcome to your program.")
+    print("I hope you enjoy your stay.")
+
+greet()
