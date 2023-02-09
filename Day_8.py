d30aed094da78ab77f2cb63d07b101d6438c80b0
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-09 21:43:11 +0330

    Update Day_8.py

--- Day_8.py
+++ Day_8.py
@@ -16,7 +16,9 @@ shift = int(input("Type the shift number:\n"))
 
 def encrypt(text, shift):
     ciphered_text = []
-    shift = len(text)
-    for letter in range(0, shift):
-        ciphered_text.append(alphabet[letter + shift])
+    for letter in text:
+        ciphered_text.append(alphabet[alphabet.index(letter) + shift])
     print(ciphered_text)
+
+
+encrypt(text, shift)
