8b807df1e65d1fd1c324c94d6d41b4fb64263394
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-11 22:12:53 +0330

    Update Day_9.py

--- Day_9.py
+++ Day_9.py
@@ -1,1 +1,14 @@
-"This is Day 9 of 100 days of code, we will learn about dictionaries and nesting in programming."
+# "This is Day 9 of 100 days of code, we will learn about dictionaries and nesting in programming."
+
+# "so, dictionaries in python are just like dictionaries in real life with some key differences."
+
+# Key -- Value
+# Bug -- An error in a program that prevents the program from running as expected.
+
+# " In python, the dictionary would look like this:"
+
+key = "bug"
+value = "An error in a program that prevents the program from running as expected"
+
+mydic = {key: value}
+print(mydic)
