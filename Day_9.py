22050725cbdf8955a5fcf6f3615dbf42f1f0813f
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-11 22:18:55 +0330

    Update Day_9.py

--- Day_9.py
+++ Day_9.py
@@ -12,3 +12,14 @@ value = "An error in a program that prevents the program from running as expecte
 
 mydic = {key: value}
 print(mydic)
+
+# This is the starting point:
+
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again.",
+
+}
+
+print(programming_dictionary["Bug"])
