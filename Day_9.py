d55c31ae03ae7ea184b13d56147b4d1b9d3c7014
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-11 22:35:03 +0330

    Update Day_9.py

--- Day_9.py
+++ Day_9.py
@@ -22,4 +22,30 @@ programming_dictionary = {
 
 }
 
+# The code below shows an example of retriving a piece of data from a dictionary
+
 print(programming_dictionary["Bug"])
+
+
+# Here's how we add items to the dictionary
+
+programming_dictionary["Parameter"] = "The values inside the actual implementation of the function."]
+
+print(programming_dictionary)
+
+
+# We often start with empty dictionary, just like empty lists, strings ...
+# Empty dictionaries are created with a set of empty curly braces
+empty_list = []
+empty_dictionary = {}
+
+# This concept can be used for wiping a dictionary! Take a look here:
+# This dictionary which had several entries, but after this argument, it's wiped out.
+
+programming_dictionary = {}
+print(programming_dictionary)
+
+# The concept of tapping into a dictionary goes beyond just adding
+# We can easily add many things to our dictionary
+
+programming_dictionary["Bug"] = "It's a very bad thing, you should avoid it."
