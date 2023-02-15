2d9710dafb6636284f644ed439ed874c7d44ea2e
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-15 22:22:21 +0330

    Create Day_10.py

---
+++ Day_10.py
@@ -0,0 +1,2 @@
+# This file contains codes from the first hundred days of Python.
+# Up to this point, 10 percent of the videos have been watched. Despite the 90 percent resting ahead, I guess I've made it far enough. 
