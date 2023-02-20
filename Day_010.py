de0a767b140f75a3fd243bc894ff9302cb0b35e5
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-20 23:55:23 +0330

    Update Day_010.py

--- Day_010.py
+++ Day_010.py
@@ -37,3 +37,22 @@ def format_name(first_name, last_name):
 
 
 print(format_name("JimMY", "CaRTEr"))
+
+
+# This coding challenge deals with modifing the leap year calculator we did before, this is challenge 10.1:
+
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap year.")
+            else:
+                print("Not leap year.")
+        else:
+            print("Leap year")
+    else:
+        print("Now leap year.")
+
+
+def days_in_month():
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
