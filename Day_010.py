df979bc3d4ba274790ec5704c67cfb78ad4fa0f3
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-25 23:24:40 +0330

    Update Day_010.py

--- Day_010.py
+++ Day_010.py
@@ -130,5 +130,13 @@ for symbol in operations:
 operation_symbol = input("Pick an operation from the line above: ")
 num2 = int(input("What's the second number? "))
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+first_answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol = input("Pick an operation from the line above: ")
+num3 = int(input("What's the second number? "))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(calculation_function(num1, num2), num3)
+
+print(f"{first_answer} {calculation_function} {num3} = {second_answer}")
