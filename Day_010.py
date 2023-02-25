8da62e406400b49c9d71b37aef7ddb6a0b04f299
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-25 22:34:08 +0330

    Update Day_010.py

--- Day_010.py
+++ Day_010.py
@@ -58,6 +58,10 @@ def is_leap(year):
 
 
 def days_in_month(year, month):
+    if month > 12 or month < 1:
+        return "Invalid month."
+    if year == 0:
+        return "Invalid year."
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if is_leap(year) and month == 2:
         return 29
