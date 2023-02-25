a3f236faf6ffff7b0ef2447afefc25a511ba3904
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-25 22:32:09 +0330

    Update Day_010.py

--- Day_010.py
+++ Day_010.py
@@ -48,17 +48,20 @@ def is_leap(year):
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
-                print("Leap year.")
+                return True
             else:
-                print("Not leap year.")
+                return False
         else:
-            print("Leap year")
+            return True
     else:
-        print("Now leap year.")
+        return False
 
 
-def days_in_month():
+def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
 
 
 year = int(input("Enter a year: "))
