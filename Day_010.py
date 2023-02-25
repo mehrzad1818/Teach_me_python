ffc4ea3d735ae128eaee480b95b503e196504f48
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-25 23:43:57 +0330

    Update Day_010.py

--- Day_010.py
+++ Day_010.py
@@ -134,9 +134,9 @@ first_answer = calculation_function(num1, num2)
 
 print(f"{num1} {operation_symbol} {num2} = {first_answer}")
 
-operation_symbol = input("Pick an operation from the line above: ")
+operation_symbol = input("Pick another operation from the line above: ")
 num3 = int(input("What's the second number? "))
 calculation_function = operations[operation_symbol]
 second_answer = calculation_function(calculation_function(num1, num2), num3)
 
-print(f"{first_answer} {calculation_function} {num3} = {second_answer}")
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
