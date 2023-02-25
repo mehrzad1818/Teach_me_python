b45218cdbe65cb783c437e1874520c983cf0ff90
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-25 23:09:41 +0330

    Update Day_010.py

--- Day_010.py
+++ Day_010.py
@@ -58,6 +58,7 @@ def is_leap(year):
 
 
 def days_in_month(year, month):
+    """It shows the number of days in the given month."""
     if month > 12 or month < 1:
         return "Invalid month."
     if year == 0:
@@ -72,3 +73,62 @@ year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(days)
+
+
+# This part talks about 'Docstrings'
+# Docstrings are basically a small pieace of information given like some sort of information to help you use the function
+
+
+# Already used functions with outputs.
+length = len(formatted_name)
+
+# Return as an early exit
+
+
+def format_name(f_name, l_name):
+    """Inputs are changed into type case letter."""
+
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"Result: {formated_f_name} {formated_l_name}"
+
+
+# Print vs Return
+# Which is which?
+
+# Take a look at this example:
+# Calculator
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What's the first number? "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("What's the second number? "))
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {answer}")
