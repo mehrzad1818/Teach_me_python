b863378f8aa716b8afcbc096bedf7ec873813ebf
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-28 23:46:40 +0330

    Update Day_011.py

--- Day_011.py
+++ Day_011.py
@@ -1,4 +1,11 @@
 # This is the 11th day of 100 days of coding
 # Your very first capstone project
+import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+playercard = cards(len(random.randint))
+cpucard = cards(len(random.randint))
+
+
+print(cpucard)
