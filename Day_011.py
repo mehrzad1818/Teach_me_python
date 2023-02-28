a843dc6712538326d69db19ef56870d86e39bd6e
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-02-28 23:41:13 +0330

    Update Day_011.py

--- Day_011.py
+++ Day_011.py
@@ -1,2 +1,4 @@
-# This is the eleventh day of one hundred days of Python.
-# Blackjack Capstone Project
+# This is the 11th day of 100 days of coding
+# Your very first capstone project
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
