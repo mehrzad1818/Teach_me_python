a553eabac78f06949cc6af275b2bb4cf0f247744
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-01 23:59:27 +0330

    Update Day_011.py

--- Day_011.py
+++ Day_011.py
@@ -2,21 +2,28 @@
 # Your very first capstone project
 import random
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 
 playercard2 = playercard.append()
 cpucard2 =
 
 
 # lists  to show
-playercard2list = []
-playercard2list = []
+
 
 def picker():
 
-    playercard = cards[random.randint(0, 12)]
-    cpucard = cards[random.randint(0, 12)]
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+    for item in playercard:
+
+        playercard2list.append(playercard)
+        cpucard2list.append(cpucard)
+
+        playercard = cards[random.randint(0, 12)]
+        cpucard = cards[random.randint(0, 12)]
+
+        playercard2list = []
+        cpucard2list = []
 
 
-
+print(cpucard2list)
