51c7662b96823b6a06e3e1cbf184d68e21140a59
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-02 23:47:31 +0330

    Update Day_011.py

--- Day_011.py
+++ Day_011.py
@@ -8,16 +8,23 @@ import random
 def picker():
 
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
     playercard = cards[random.randint(0, 12)]
     cpucard = cards[random.randint(0, 12)]
+    playercard2 = cards[random.randint(0, 12)]
+    cpucard2 = cards[random.randint(0, 12)]
 
-    playercard2list = []
-    playercard2list.append(playercard)
-    print(playercard2list)
+    playercardlist = []
 
-    cpucard2list = []
-    cpucard2list.append(cpucard)
-    print(cpucard2list)
+    playercardlist.append(playercard)
+    playercardlist.append(playercard2)
+    print(playercardlist)
+
+    cpucardlist = []
+
+    cpucardlist.append(cpucard)
+    cpucardlist.append(cpucard2)
+    print(cpucardlist)
 
 
 picker()
