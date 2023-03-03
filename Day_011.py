b9feb9b0f63cee38ea29a1b65f24b6b29b31b8cb
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-03 22:08:34 +0330

    Update Day_011.py

--- Day_011.py
+++ Day_011.py
@@ -3,30 +3,35 @@
 import random
 
 # lists  to show
+playing_consent = input(
+    str("Do you want to play a game of Blackjack? Type 'y' or 'n': "))
 
+if playing_consent == "y":
 
-def picker():
+    def picker():
 
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+        cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-    playercard = cards[random.randint(0, 12)]
-    cpucard = cards[random.randint(0, 12)]
-    playercard2 = cards[random.randint(0, 12)]
-    cpucard2 = cards[random.randint(0, 12)]
+        playercard = cards[random.randint(0, 12)]
+        cpucard = cards[random.randint(0, 12)]
+        playercard2 = cards[random.randint(0, 12)]
+        cpucard2 = cards[random.randint(0, 12)]
 
-    playercardlist = []
+        playercardlist = []
 
-    playercardlist.append(playercard)
-    playercardlist.append(playercard2)
+        playercardlist.append(playercard)
+        playercardlist.append(playercard2)
 
-    print(playercardlist)
+        print(
+            f"The player's cards are {playercardlist}. The total is {playercard + playercard2}.")
 
-    cpucardlist = []
+        cpucardlist = []
 
-    cpucardlist.append(cpucard)
-    cpucardlist.append(cpucard2)
+        cpucardlist.append(cpucard)
+        cpucardlist.append(cpucard2)
 
-    print(cpucardlist)
+        print(
+            f"The dealer's cards are {cpucardlist}. The total is {cpucard + cpucard2}.")
 
 
 picker()
