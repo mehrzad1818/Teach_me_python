535842cd7c99aabdf199c16b249a1af55cf0edd3
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-05 23:53:49 +0330

    Update Day_011.py

--- Day_011.py
+++ Day_011.py
@@ -90,6 +90,9 @@ else:
 # which is correct and contains important notes
 
 
+import random
+
+
 def deal_card():
     """Returns a random card from the deck."""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -101,5 +104,33 @@ user_cards = []
 computer_cards = []
 
 for _ in range(2):
-    new_card = deal_card()
-    user_cards.append(new_card)
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+
+def coalculate_score(cards):
+
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+
+user_score = calculate_score(user_cards)
+computer_score = coalculate_score(computer_cards)
+print(f"    Your cards: {user_cards}, current score: {user_score}")
+print(f"    Computer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
+else:
+    user_should_deal = input(
+        "Type 'y' to get another card, type 'n' to pass: ")
+    if user_should_deal == "y":
+        user_cards.append(deal_card())
+    else:
+        is_game_over = True
