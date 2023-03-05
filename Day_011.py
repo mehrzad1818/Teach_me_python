c5dda8695510de989869f096cd003bd91376f87c
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-05 23:52:48 +0330

    Update Day_011.py

--- Day_011.py
+++ Day_011.py
@@ -88,7 +88,6 @@ else:
 
 # This part deals with the refined version of the video,
 # which is correct and contains important notes
-import random
 
 
 def deal_card():
@@ -102,26 +101,5 @@ user_cards = []
 computer_cards = []
 
 for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-
-
-def coalculate_score(cards):
-
-    if sum(cards) == 21 and len(cards) == 2:
-        return 0
-
-    if 11 in cards and sum(cards) > 21:
-        cards.remove(11)
-        cards.append(1)
-
-    return sum(cards)
-
-
-user_score = calculate_score(user_cards)
-computer_score = coalculate_score(computer_cards)
-print(f"    Your cards: {user_cards}, current score: {user_score}")
-print(f"    Computer's first card: {computer_cards[0]}")
-
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
+    new_card = deal_card()
+    user_cards.append(new_card)
