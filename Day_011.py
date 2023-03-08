919117e55fbc59fd251480b2ffebd0028187bfed
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-08 19:28:16 +0330

    Update Day_011.py

--- Day_011.py
+++ Day_011.py
@@ -1,6 +1,7 @@
 # This is the 11th day of 100 days of coding
 # Your very first capstone project
 import random
+from replit import clear
 
 blackjacklogo = """
 
@@ -96,62 +97,71 @@ def deal_card():
     return card
 
 
-user_cards = []
-computer_cards = []
+def play_game():
 
-for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
+    print(blackjacklogo)
 
+    user_cards = []
+    computer_cards = []
 
-def calculate_score(cards):
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
 
-    if sum(cards) == 21 and len(cards) == 2:
-        return 0
+    def calculate_score(cards):
 
-    if 11 in cards and sum(cards) > 21:
-        cards.remove(11)
-        cards.append(1)
+        if sum(cards) == 21 and len(cards) == 2:
+            return 0
 
-    return sum(cards)
+        if 11 in cards and sum(cards) > 21:
+            cards.remove(11)
+            cards.append(1)
 
+        return sum(cards)
 
-def compare(user_score, computer_score):
-    if user_score == computer_score:
-        return "Draw"
-    elif computer_score == 0:
-        return "Lose, opponent has Blackjack."
-    elif user_score == 0:
-        return "Win, you have Blackjack."
-    elif user_score > 21:
-        return "You went over. You lost."
-    elif computer_score > 21:
-        return "Opponent went over. You won."
-    elif user_score > computer_score:
-        return "You win."
-    else:
-        return "You lose."
-
-
-while not is_game_over:
-
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(computer_cards)
-    print(f"    Your cards: {user_cards}, current score: {user_score}")
-    print(f"    Computer's first card: {computer_cards[0]}")
-
-    if user_score == 0 or computer_score == 0 or user_score > 21:
-        is_game_over = True
-    else:
-        user_should_deal = input(
-            "Type 'y' to get another card, type 'n' to pass: ")
-        if user_should_deal == "y":
-            user_cards.append(deal_card())
+    def compare(user_score, computer_score):
+        if user_score == computer_score:
+            return "Draw"
+        elif computer_score == 0:
+            return "Lose, opponent has Blackjack."
+        elif user_score == 0:
+            return "Win, you have Blackjack."
+        elif user_score > 21:
+            return "You went over. You lost."
+        elif computer_score > 21:
+            return "Opponent went over. You won."
+        elif user_score > computer_score:
+            return "You win."
         else:
+            return "You lose."
+
+    while not is_game_over:
+
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"    Your cards: {user_cards}, current score: {user_score}")
+        print(f"    Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
             is_game_over = True
+        else:
+            user_should_deal = input(
+                "Type 'y' to get another card, type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
 
-while computer_score != 0 and computer_score < 17:
-    computer_cards.append(deal_card())
-    computer_score = calculate_score
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score
 
-print(compare(user_score, computer_score))
+    print(f"    Your final hand: {user_cards}, final score: {user_score}")
+    print(
+        f"    Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(user_score, computer_score))
+
+
+while input("Do you want to play one more match? Type 'y' or 'n'.") == "y":
+    clear()
+    play_game()
