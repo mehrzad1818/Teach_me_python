3b06b4e94d3cf267dc0de89a8bb9691e2a7858c2
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-08 19:35:09 +0330

    Update Day_012.py

--- Day_012.py
+++ Day_012.py
@@ -1,1 +1,23 @@
 # This is the 12th day of Learning Python
+# We will learn about Global and Local Namespaces and also Scopes
+
+# What is Scope?
+
+# Scope is like planting an apple tree in your house.
+# If it is planted inside your garden with fences around it,
+# it is you and your family which can only access it.
+
+# But it it's on the sidewalk, it is accessed by everyone.
+
+# This example below will illustrate the points:
+
+enemies = 1
+
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
