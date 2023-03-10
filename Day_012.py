ec93a79a7a94d5e6b6251f437e86d269a5744016
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-10 23:32:28 +0330

    Update Day_012.py

--- Day_012.py
+++ Day_012.py
@@ -76,9 +76,85 @@ enemies = 1
 
 
 def increase_enemies():
-    enemies = 2
+    global enemies
+    enemies += 2
     print(f"enemies inside function: {enemies}")
 
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# So, the above method, which is changing global scope values is not recommended since it is prone to make mistakes and code breaks
+# What do we do instead?
+
+enemies = 1
+
+
+def increase_enemies():
+    print(f"enemies inside function: {enemies}")
+    return enemies + 1
+
+
+enemies = increase_enemies()
+print(f"enemies outside functino: {enemies}")
+
+# Global Scopes have their own usage, too
+
+# Global Constants
+# They are used for variable which are not going to be modified ever again in the code.
+# The convention is to name them in all uppercase to recognize them in the code
+
+
+PI = 3.14159
+URL = "https://www.google.com"
+TWITTER_HANDLE = "nameme"
+
+
+def calc():
+    PI
+
+
+# Number Guessing Game
+
+import random
+logo = """
+   ___                       _____                  __                 _
+  / _ \_   _  ___  ___ ___  /__   \ |__   ___    /\ \ \_   _ _ __ ___ | |__   ___ _ __
+ / /_\/ | | |/ _ \/ __/ __|   / /\/ '_ \ / _ \  /  \/ / | | | '_ ` _ \| '_ \ / _ \ '__|
+/ /_\| |_| |  __/\__ \__ \  / /  | | | |  __/ / /\  /| |_| | | | | | | |_) |  __/ |
+\____/ \__,_|\___||___/___/  \/   |_| |_|\___| \_\ \/  \__,_|_| |_| |_|_.__/ \___|_|
+
+"""
+
+
+answer = random.randint(1, 100)
+
+print(logo)
+print("Welcome to the Number Guessing Game!\n")
+print("I'm thinking of a number between 1 and 100.")
+print(f"Psssst, the correct answer is {answer}")
+
+HARD_MODE = 5
+EASY_MODE = 10
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+if difficulty == "hard":
+    print(f"You have {HARD_MODE} attempts remaining to guess the number.")
+elif difficulty == "easy":
+    print(f"You have {EASY_MODE} attempts remaining to guess the number.")
+
+
+def compare(difficulty, answer, guess):
+
+    guess = input("What is your guess?\n")
+
+    if answer > guess:
+        print("Too low.")
+    if guess > answer:
+        print("Too high.")
+    if guess == answer:
+        print("You guessed right.")
+
+
+compare(difficulty, answer, guess)
