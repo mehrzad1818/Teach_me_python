e645b5b17b8f27e5c45ced4f5debca81f12e467e
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-13 23:56:23 +0330

    Create Day_012.py

--- Day_012.py
+++ Day_012.py
@@ -166,3 +166,25 @@ def compare(difficulty, answer, guess):
 
 
 compare(difficulty, answer, guess)
+
+# Completed Version
+
+
+from random import randint
+
+
+def check_answer(guess, answer):
+    if guess > answer:
+        print("Too high.")
+    elif guess < answer:
+        print("Too low.")
+    else:
+        print(f"You got it! The answer was {answer}.")
+
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+answer = randint(1, 100)
+
+guess = int(input("Make a guess: "))
+
