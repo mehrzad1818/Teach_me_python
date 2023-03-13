bcfd5b3b6d679cf5569161d457675f25984a1ab2
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-14 00:08:29 +0330

    Update Day_012.py

--- Day_012.py
+++ Day_012.py
@@ -11,6 +11,7 @@
 
 # This example below will illustrate the points:
 
+from random import randint
 import random
 enemies = 1
 
@@ -170,7 +171,8 @@ compare(difficulty, answer, guess)
 # Completed Version
 
 
-from random import randint
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 
 
 def check_answer(guess, answer):
@@ -182,9 +184,24 @@ def check_answer(guess, answer):
         print(f"You got it! The answer was {answer}.")
 
 
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == "easy":
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+
+set_difficulty()
+
+
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 answer = randint(1, 100)
+print(f"Pssst! The correct answer is {answer}.")
+
+turns = set_difficulty
+print(f"You have {turns} attempts remaining to guess the number.")
+
 
 guess = int(input("Make a guess: "))
-
