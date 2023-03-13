f79d01a4dc51987088cac2515698f16f0317cc17
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-14 00:49:20 +0330

    Update Day_012.py

--- Day_012.py
+++ Day_012.py
@@ -175,11 +175,14 @@ EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
 
-def check_answer(guess, answer):
+def check_answer(guess, answer, turns):
+    """Checks answer agains guess. Returns the number of turns remaining."""
     if guess > answer:
         print("Too high.")
+        return turns - 1
     elif guess < answer:
         print("Too low.")
+        return turns - 1
     else:
         print(f"You got it! The answer was {answer}.")
 
@@ -199,15 +202,18 @@ def game():
     print(f"Pssst! The correct answer is {answer}.")
 
     turns = set_difficulty
-    print(f"You have {turns} attempts remaining to guess the number.")
 
     guess = 0
 
     while guess != answer:
-
+        print(f"You have {turns} attempts remaining to guess the number.")
         guess = int(input("Make a guess: "))
-
-        check_answer(guess, answer)
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run out of guesses. You lose.")
+            return
+        elif guess != answer:
+            print("Guess again.")
 
 
 game()
