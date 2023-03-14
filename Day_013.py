37526a7df3688e37962f76b615a7e16e187322c0
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-14 23:57:12 +0330

    Update Day_013.py

--- Day_013.py
+++ Day_013.py
@@ -3,3 +3,17 @@
 # Debugging points
 
 # Grace Hopper found the first bug.
+# Everyone gets bugs.
+
+
+# 1. Describe th Problem:
+#       you should be able to make sense of what is wrong.
+
+
+def my_function():
+    for i in range(1, 20):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
