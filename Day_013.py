32fdb767ac41ef49517ae7877bd617b7feacd0df
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-15 19:53:07 +0330

    Update Day_013.py

--- Day_013.py
+++ Day_013.py
@@ -197,7 +197,7 @@ else:
 
 # Fixed Code
 
-year = input("Which year do you want to check?\n")
+year = int(input("Which year do you want to check?\n"))
 
 if year % 4 == 0:
     if year % 100 == 0:
@@ -209,3 +209,40 @@ if year % 4 == 0:
         print("Leap year.")
 else:
     print("Not leap year.")
+
+# Day 13.3 - Debug Fizz Buzz Exercise
+
+# Guides and rules:
+
+# 1. Your program should print each number from 1 to 100 in turn.
+# 2. When the number is divisible by 3, then instead of printing the number it should print "Fizz".
+# 3. When the number is divisible by 5, then instead of printing the number it should print "Buzz".
+# 4. And if the number is divisible by both 3 and 5 e.g., 15 then instead of the number it should print "FizzBuzz".
+
+# Broken Code
+
+for number in range(1, 101):
+    if number % 3 == 0 or number % 5 == 0:
+        print("FizzBuzz")
+    if number % 3 == 0:
+        print("Fizz")
+    if number % 5 == 0:
+        print("Buzz")
+    else:
+        print([number])
+
+# Fixed Code
+
+for number in range(1, 101):
+
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+
+    elif number % 3 == 0:
+        print("Fizz")
+
+    elif number % 5 == 0:
+        print("Buzz")
+
+    else:
+        print(number)
