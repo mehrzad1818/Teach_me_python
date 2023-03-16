8976a30a691d837c38a3846800ef6d31a0519ea4
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-16 11:21:39 +0330

    Create Day_014.py

---
+++ Day_014.py
@@ -0,0 +1,26 @@
+# This is Day 14 of 100 Days of coding.
+#
+# Today's project is to make a game named "Higher or Lower"
+#   This game compares two situation and gives us an output.
+
+main_logo = """
+    __  __    _              __                
+   / / / /   (_)   ____ _   / /_   ___    _____
+  / /_/ /   / /   / __ `/  / __ \ / _ \  / ___/
+ / __  /   / /   / /_/ /  / / / //  __/ / /    
+/_/ /_/   /_/    \__, /  /_/ /_/ \___/ /_/     
+    __          /____/                         
+   / /   ____  _      __  ___    _____         
+  / /   / __ \| | /| / / / _ \  / ___/         
+ / /___/ /_/ /| |/ |/ / /  __/ / /             
+/_____/\____/ |__/|__/  \___/ /_/              
+                                               
+"""
+versus_symbol = """
+                      
+`7M'   `MF',pP"Ybd    
+  VA   ,V  8I   `"    
+   VA ,V   `YMMMa.    
+    VVV    L.   I8 ,, 
+     W     M9mmmP' db 
+"""
