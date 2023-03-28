5879c047d968bca6fbd3164b79c35790d06cb002
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-03-28 23:40:19 +0330

    Update Day_016.py

--- Day_016.py
+++ Day_016.py
@@ -1,1 +1,13 @@
+# This is Day 16 of 100 Days of Code.
+# "Today we are learning about "Object Oriented Programming.""
 
+# In Procedure Programming which we've been doing so far,
+# all of the components are connected together from disparate areas.
+
+# OOP is like assigning a group of people to run a restaurant.
+# In PRocedural programming, you are supposed to be the "ONE" person who
+# does all of the things, but in OOP, you are just the manager looking at your staff
+# without telling them what to do, because they are already trained for it.
+
+
+# How to use OOP????
