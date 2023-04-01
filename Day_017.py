7fd8ca64c79fd0c70036c0bd5a00452c989c6ad8
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-04-01 23:15:57 +0330

    Create Day_017.py

--- Day_017.py
+++ Day_017.py
@@ -1,1 +1,18 @@
 
+class User:
+    """ Manages the user. """
+    pass
+
+
+user_1 = User()
+user_1.id = "001"
+user_1.username = "Jimmy"
+
+print(user_1.username)
+
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "Jones"
+
+print(user_2.username)
