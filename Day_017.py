2fd0c42a7661b918d8a23f826731092001ae16eb
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-04-01 23:25:00 +0330

    Update Day_017.py

--- Day_017.py
+++ Day_017.py
@@ -1,18 +1,17 @@
-
 class User:
     """ Manages the user. """
-    pass
+
+    def __init__(self) -> None:
+        print("New user is being created ... ")
 
 
 user_1 = User()
 user_1.id = "001"
 user_1.username = "Jimmy"
-
 print(user_1.username)
 
 
 user_2 = User()
 user_2.id = "002"
 user_2.username = "Jones"
-
 print(user_2.username)
