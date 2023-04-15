1de6b53f6ca00da66989707719ba8a4bc4a3fc8a
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-04-15 12:33:20 +0330

    Update Day_019.py

--- Day_019.py
+++ Day_019.py
@@ -32,3 +32,46 @@ screen.exitonclick()
 
 # def function_b(function_a):
 #     # Do this
+
+
+
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    """ Tim goes forwaaarrrddd. """
+    tim.forward(10)
+
+
+def move_backward():
+    """ Tim goes backwaaarrrddd. """
+    tim.backward(10)
+
+
+def yaw_clockwise():
+    """ Tim goes weeeeeee. """
+    tim.right(5)
+
+
+def yaw_c_clockwise():
+    """ Tim goes wooooooo. """
+    tim.left(5)
+
+
+def reset():
+    """ Tim goes baaaaaaaaack. """
+    tim.reset()
+
+
+screen.listen()
+screen.exitonclick
+
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="d", fun=yaw_clockwise)
+screen.onkey(key="a", fun=yaw_c_clockwise)
+screen.onkey(key="c", fun=reset)
+
