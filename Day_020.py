0bd7a8a304dc93a86dc5ed48c73c57e74abff0ff
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-04-16 23:15:23 +0330

    Update Day_020.py

--- Day_020.py
+++ Day_020.py
@@ -1,1 +1,24 @@
+# This is the day 20 of 100 days of Python.
+# We're going to make a snake game in two days.'
 
+# The problems are as follow:
+
+# 1. Create a snake body
+# 2. Move the snake
+# 3. Create snake food
+# 4. Detect collision with food
+# 5. create a scoreboard
+# 6. detect collision with wall
+# 7. detect collision with tail
+
+
+from turtle import Screen, Turtle
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+
+screen.exitonclick()
