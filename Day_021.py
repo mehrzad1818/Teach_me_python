52305940e73685207da09c3cab1e69af3bc61326
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-05-06 23:55:36 +0330

    Update Day_021.py

--- Day_021.py
+++ Day_021.py
@@ -1,2 +1,15 @@
 # This file contains the continue of Python Snake Prject
 # This project follows the instructions given on the 21st day of 100days of Python
+
+
+from turtle import Turtle, Screen
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+x_positions = [0, -20, -40]
+all_snakes_body = []
