bd31e07dcee9decaabb560179775fa5e224694a5
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-05-06 23:55:49 +0330

    Update Day_021.py

--- Day_021.py
+++ Day_021.py
@@ -4,10 +4,3 @@
 
 from turtle import Turtle, Screen
 import time
-
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
-screen.tracer(0)
-
