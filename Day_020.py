b1bb244ddd88ba25e2e2ba14927a3f903b3c7304
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-07-05 12:52:19 +0330

    Update Day_020.py

--- Day_020.py
+++ Day_020.py
@@ -21,28 +21,34 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-coordinates = [(0, 0), (-20, 0), (-40, 0)]
+x_positions = [0, -20, -40]
 all_snakes_body = []
 
 
-for turtle_index in coordinates:
+for turtle_index in range(0, 3):
     snakes_body = Turtle(shape="square")
-    snakes_body.color("white")
     snakes_body.penup()
-    snakes_body.goto(turtle_index)
+    snakes_body.color("white")
+    snakes_body.goto(x=x_positions[turtle_index], y=0)
     all_snakes_body.append(snakes_body)
 
 
-game_is_on = True
+GAME_IS_ON = True
 
-while game_is_on:
+while GAME_IS_ON:
     screen.update()
     time.sleep(0.1)
-    for snakes in all_snakes_body:
-        snakes.forward(20)
 
 
-all_snakes_body[0].left(90)
+    for snakes_part in range(len(all_snakes_body) - 1, 0, -1):
+        new_x = all_snakes_body[snakes_part - 1].xcor()
+        new_y = all_snakes_body[snakes_part - 1].ycor()
+        all_snakes_body[snakes_part].goto(new_x, new_y)
+
+
+    # for snakes in all_snakes_body:
+    #     snakes.forward(20)
+    # all_snakes_body[0].left(90)
 
 
 
