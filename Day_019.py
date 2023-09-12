0b1ace9c76283544171e22d733c5f4cd0bac0439
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-09-12 22:34:38 +0330

    Update Day_019.py

--- Day_019.py
+++ Day_019.py
@@ -2,8 +2,10 @@
 
 
 # This module uses event listener to trigger a funciton based on the given input (real-time input).
-
+# %%
+import random
 from turtle import Turtle, Screen
+
 # import random
 
 tim = Turtle()
@@ -11,7 +13,7 @@ screen = Screen()
 
 
 def move_forward():
-    """ Responsible for moving Tim on the screen. """
+    """Responsible for moving Tim on the screen."""
     tim.forward(10)
 
 
@@ -22,7 +24,7 @@ screen.exitonclick()
 # This part is like some sort of combining different functions together.
 # We pass in the outcome of one function into income of the other function.
 
-
+# %%
 # def function_a(something):
 
 #     # Do this with something
@@ -32,9 +34,8 @@ screen.exitonclick()
 
 # def function_b(function_a):
 #     # Do this
-
-
-
+# %%
+import random
 from turtle import Turtle, Screen
 
 tim = Turtle()
@@ -42,27 +43,27 @@ screen = Screen()
 
 
 def move_forward():
-    """ Tim goes forwaaarrrddd. """
+    """Tim goes forwaaarrrddd."""
     tim.forward(10)
 
 
 def move_backward():
-    """ Tim goes backwaaarrrddd. """
+    """Tim goes backwaaarrrddd."""
     tim.backward(10)
 
 
 def yaw_clockwise():
-    """ Tim goes weeeeeee. """
+    """Tim goes weeeeeee."""
     tim.right(5)
 
 
 def yaw_c_clockwise():
-    """ Tim goes wooooooo. """
+    """Tim goes wooooooo."""
     tim.left(5)
 
 
 def reset():
-    """ Tim goes baaaaaaaaack. """
+    """Tim goes baaaaaaaaack."""
     tim.reset()
 
 
@@ -76,26 +77,22 @@ screen.onkey(key="d", fun=yaw_clockwise)
 screen.onkey(key="a", fun=yaw_c_clockwise)
 screen.onkey(key="c", fun=reset)
 
-
-
+# %%
 # This is turtle race challenge:
-
-from turtle import Turtle, Screen
-import random
 
 
 screen = Screen()
 screen.setup(width=500, height=400)
 is_race_on = False
-user_bet = screen.textinput(title="Make your bet!",
-                            prompt="Which turtle will win the race? Enter a color.")
+user_bet = screen.textinput(
+    title="Make your bet!", prompt="Which turtle will win the race? Enter a color."
+)
 
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-100, -60, -20, +20, +60, +100]
 all_turtles = []
 
 for turtle_index in range(0, 6):
-
     new_turtle = Turtle(shape="turtle")
     new_turtle.penup()
     new_turtle.color(colors[turtle_index])
@@ -108,7 +105,6 @@ if user_bet:
 
 
 while is_race_on:
-
     for turtle in all_turtles:
         if turtle.xcor() > 230:
             is_race_on = False
@@ -123,4 +119,3 @@ while is_race_on:
 
 
 screen.exitonclick()
-
